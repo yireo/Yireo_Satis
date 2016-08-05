88e9a2c16ddea75cf9315fa5a880701f77c63325
Yireo <info@yireo.com>
2016-08-05 14:15:17 +0200

    Updated satis

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd satis/
+git pull origin master
+git pull github master
 php bin/satis build ../satis.json ..
 cd ..
 git add include/
-git commit -m 'Updated satis'
+git commit -m 'Updated satis' .
 git push origin master
 git push github master
